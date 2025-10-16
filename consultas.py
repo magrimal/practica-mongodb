2d52d26e0b4b3edcf55b32ed3b02ce404ab62fb4
natalia-123 <nlarosa@ucm.es>
2025-10-16 18:23:37 +0200

    archivos para la pracitca

---
+++ consultas.py
@@ -0,0 +1,43 @@
+"""
+Asignatura: SGDI
+Práctica 2:
+Autores: XXXXXXXXXXXXXXX
+
+Declaración de integridad
+Declaramos que esta solución es fruto exclusivamente de nuestro trabajo 
+personal. No hemos sido ayudados por ninguna otra persona o sistema automático
+ni hemos obtenido la solución de fuentes externas, y tampoco hemos compartido 
+nuestra solución con otras personas de manera directa o indirexcta. 
+Declaramos además que no hemos realizado de manera deshonesta ninguna otra 
+actividad que pueda mejorar nuestros resultados ni perjudicar los resultados 
+de los demás.
+"""
+
+
+def usuario_peliculas(mongoclient, user_id, n):
+    ...
+
+
+def usuarios_gustos(mongoclient, gustos, n):
+    ...
+
+  
+def usuario_sexo_edad(mongoclient, sexo, edad_min, edad_max ):
+    ...
+
+    
+def usuarios_apellidos(mongoclient, ):
+    ...
+    
+    
+def pelicula_prefijo(mongoclient, prefijo):
+    ...
+    
+
+def usuarios_gustos_numero(mongoclient, n):
+    ...
+    
+
+def usuarios_vieron_pelicula(mongoclient, id_pelicula, inicio, fin):
+    ...
+
---
+++ geo.py
@@ -0,0 +1,25 @@
+"""
+Asignatura: SGDI
+Práctica 2:
+Autores: XXXXXXXXXXXXXXX
+
+Declaración de integridad
+Declaramos que esta solución es fruto exclusivamente de nuestro trabajo 
+personal. No hemos sido ayudados por ninguna otra persona o sistema automático
+ni hemos obtenido la solución de fuentes externas, y tampoco hemos compartido 
+nuestra solución con otras personas de manera directa o indirexcta. 
+Declaramos además que no hemos realizado de manera deshonesta ninguna otra 
+actividad que pueda mejorar nuestros resultados ni perjudicar los resultados 
+de los demás.
+"""
+
+
+def agg_clean(mongoclient):
+    ...
+
+def geo_query1(mongoclient, n):
+    ...
+
+def geo_query2(mongoclient):
+    ...
+
