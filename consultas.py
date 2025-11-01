22fae0b72dd421742e12b5fdf97a59abe8d77514
natalia-123 <nlarosa@ucm.es>
2025-11-01 11:09:04 +0100

    correcciones ejercicio 1

--- consultas.py
+++ consultas.py
@@ -33,167 +33,61 @@ def obtener_peliculas(mongoclient):
 def usuario_peliculas(mongoclient, user_id, n):
     usuarios = obtener_usuarios(mongoclient)
 
-    pipeline = [
-        {"$limit": n}
-    ]
+    usuariosPeliculas = usuarios.find({"_id": user_id}, {"visualizaciones": {"$slice":n}, "_id":0,"visualizaciones":1, "email":1})
 
-    resultadoLimiteN = usuarios.aggregate(pipeline)
-    
-    pipeline = [
-        {"$match": {"_id": user_id}},
-        {"$project": {"_id": 0, "email": 1}}
-    ]
+    return list(usuariosPeliculas)
 
-    resultadoFiltroUser = usuarios.aggregate(pipeline)
-    
-    data = {
-        "visualizaciones": list(resultadoLimiteN),
-    }
-    lista = [data, list(resultadoFiltroUser)]; # No se imprime exactamente igual que en el PDF
-
-    return lista
 
 def usuarios_gustos(mongoclient, gustos, n):
     usuarios = obtener_usuarios(mongoclient)
 
-    pipeline = [
-        {"$match": {"gustos": { "$in": gustos } }},
-        {"$project": {"_id": 1, "apellido1": 1, "apellido2": 1, "nombre": 1}},
-        {"$limit": n}
-    ]
+    usuariosGustos = usuarios.find({"gustos": {"$all": gustos}}, {"_id":1,"apellido1":1,"apellido2":1,"nombre":1}).limit(n)
 
-    resultado = usuarios.aggregate(pipeline) # apellido1, apellido2 y nombre no se imprime en el orden del PDF
-    return list(resultado)
+    return list(usuariosGustos) #¿en el pdf imprime 3 y son 5?
+
 
 def usuario_sexo_edad(mongoclient, sexo, edad_min, edad_max ):
     usuarios = obtener_usuarios(mongoclient)
 
-    pipeline = [
-        {"$match": {"sexo": sexo, "edad": { "$gt": edad_min - 1, "$lt": edad_max + 1} }},
-        {"$project": {"_id": 1}}
-    ]
+    usuariosSexoEdad = usuarios.find({"sexo": sexo, "edad": { "$gt": edad_min - 1, "$lt": edad_max + 1}}, {"_id:1"})
 
-    resultado = usuarios.aggregate(pipeline) # apellido1, apellido2 y nombre no se imprime en el orden del PDF
-    return list(resultado)
-
+    return list(usuariosSexoEdad)
 
 def usuarios_apellidos(mongoclient):
     usuarios = obtener_usuarios(mongoclient)
 
-    pipeline = [
-    {
-        '$match': {
-            '$expr': {
-                '$eq': [
-                    '$apellido1', '$apellido2'
-                ]
-            }
-        }
-    }, {
-        '$sort': {
-            'edad': 1
-        }
-    }, {
-        '$project': {
-            '_id': 0, 
-            'apellido1': 1, 
-            'apellido2': 1, 
-            'nombre': 1
-        }
-    }
-    ]
-
-    resultado = usuarios.aggregate(pipeline)
-    return list(resultado)
+    usuariosApellidos = usuarios.find({"$expr": { "$eq": ["$apellido1", "$apellido2"]}}, {"_id":0,"apellido1":1,"apellido2":1,"nombre":1}).sort({"edad":1})
+    
+    return list(usuariosApellidos) #apellido1 y apellido2 no se imprimen en el mismo orden que el pdf
     
 
 def pelicula_prefijo(mongoclient, prefijo):
     peliculas = obtener_peliculas(mongoclient)
 
-    pipeline = [
-    {
-        '$match': {
-            'director': re.compile(f"^{re.escape(prefijo)}")
-        }
-    }, {
-        '$project': {
-            '_id': 0, 
-            'director': 1, 
-            'titulo': 1
-        }
-    }
-    ]
+    peliculaPrefijo = peliculas.find({"director": re.compile(f"^{re.escape(prefijo)}")}, {"_id":0,"director":1,"titulo":1})
 
-    resultado = peliculas.aggregate(pipeline)
-    return list(resultado)
+    return list(peliculaPrefijo)
     
 
 def usuarios_gustos_numero(mongoclient, n):
     usuarios = obtener_usuarios(mongoclient)
 
-    pipeline = [
-    {
-        '$match': {
-            'gustos': {
-                '$size': 3
-            }
-        }
-    }, {
-        '$sort': {
-            'edad': -1
-        }
-    }, {
-        '$project': {
-            'edad': 1, 
-            'gustos': 1
-        }
-    }
-]
+    usuariosGustosNumero = usuarios.find({"gustos":{"$size":n}}, {"edad":1,"gustos":1}).sort({"edad":-1})
 
-    resultado = usuarios.aggregate(pipeline)
-    return list(resultado)
+    return list(usuariosGustosNumero) #edad se imprime como tercer elemento, a diferencia del pdf que se imprime en el segundo
     
 
 def usuarios_vieron_pelicula(mongoclient, id_pelicula, inicio, fin):
     usuarios = obtener_usuarios(mongoclient)
 
-    fecha_inicio = datetime.datetime.strptime(inicio, "%Y-%m-%d").replace(tzinfo=datetime.timezone.utc)
-    fecha_fin = datetime.datetime.strptime(fin, "%Y-%m-%d").replace(tzinfo=datetime.timezone.utc)
+    usuariosVieronPeliculas = usuarios.find({"visualizaciones": { "$elemMatch": {"fecha": { "$gte": inicio, "$lt": fin } ,"_id": ObjectId(id_pelicula)} }}, {"_id": 1 })
 
-    pipeline = [
-    {
-        '$unwind': '$visualizaciones'
-    }, {
-        '$project': {
-            'visualizaciones.fecha': {
-                '$dateFromString': {
-                    'dateString': '$visualizaciones.fecha'
-                }
-            }, 
-            'visualizaciones._id': 1
-        }
-    }, {
-        '$match': {
-            'visualizaciones.fecha': {
-                '$gt': fecha_inicio,
-                '$lt': fecha_fin
-            }, 
-            'visualizaciones._id': ObjectId(id_pelicula)
-        }
-    }, {
-        '$project': {
-            '_id': 1
-        }
-    }
-]
-
-    resultado = usuarios.aggregate(pipeline)
-    return list(resultado)
+    return list(usuariosVieronPeliculas) #orden de impresión no es igual al pdf
 
 #print(usuario_peliculas(mongoclient, "gnoguera", 3))
-#print(usuarios_gustos(mongoclient, ["terror", "comedia"], 5))
-#print(usuario_sexo_edad(mongoclient, "M", 39, 54 ))
+print(usuarios_gustos(mongoclient, ["terror", "comedia"], 5))
+#print(usuario_sexo_edad(mongoclient, "M", 50, 80 ))
 #print(usuarios_apellidos(mongoclient))
 #print(pelicula_prefijo(mongoclient, "Yol"))
-#print(usuarios_gustos_numero(mongoclient, 3))
-print(usuarios_vieron_pelicula(mongoclient, '583ef652323e9572e2814c48',  "1999-01-01", "2002-12-31"))
+#print(usuarios_gustos_numero(mongoclient, 6))
+#print(usuarios_vieron_pelicula(mongoclient, '583ef652323e9572e2814c48',  "1999-01-01", "2002-12-31"))
